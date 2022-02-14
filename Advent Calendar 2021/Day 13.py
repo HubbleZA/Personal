cae5f29ade5f1ccc46d9fa9129c8e7f709dfe720
HubbleZA <rladeira95@gmail.com>
2022-02-14 23:31:14 +0200

    Day 13

--- Advent Calendar 2021/Day 13.py
+++ Advent Calendar 2021/Day 13.py
@@ -1,4 +1,25 @@
 with open('Day_13_Input.txt', "r") as f:
     myinput = (f.read().splitlines())
 
-print(myinput)
+instructions = []
+points = []
+
+for i in range(len(myinput)):
+    if myinput[i] == '':
+        points = ((myinput[0:i]).copy())
+        instructions = ((myinput[(i+1):len(myinput)]).copy())
+
+print(points)
+print(instructions)
+
+x = []
+y = []
+
+for i in points:
+    a,b = i.split(',')
+    x.append(int(a))
+    y.append(int(b))
+
+map = [[0]*(max(x))]*max(y)
+
+
