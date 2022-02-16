a552777b18af6f37721a3ae2d0f54e8413db01f7
HubbleZA <rladeira95@gmail.com>
2022-02-16 23:37:12 +0200

    Day 13

--- Advent Calendar 2021/Day 13.py
+++ Advent Calendar 2021/Day 13.py
@@ -20,6 +20,68 @@ for i in points:
     x.append(int(a))
     y.append(int(b))
 
-map = [[0]*(max(x))]*max(y)
+
+map = [[0for i in range((max(x))+1)] for j in range((max(y))+1)]
+
+for i in range(len(x)):
+    (map[(y[i])][(x[i])]) = 1
 
 
+
+def fold(axis, line, map):
+    fmap = []
+    if axis == 'x':
+        for i in range(len(map)):
+            fmap = (map[i][line:])
+            map[i] = (map[i][0:line])
+            print(len(fmap))
+            a = (fmap[i][::-1])
+            b = map[i]
+            for k in range(len(a)):
+                if a[k-1] + b[k-1] > 0:
+                    map[i][k-1] = 1
+
+    elif axis == 'y':
+        fmap = map[line:]
+        map = (map[0:line])
+        print(len(map))
+        print(len(fmap))
+        while len(fmap) > len(map):
+            fmap = fmap[0:len(fmap)-1]
+
+        for i in range(len(fmap)):
+            a = (fmap[(len(fmap)-1)-i])
+            b = map[i]
+            for k in range(len(a)):
+                if a[k-1] + b[k-1] > 0:
+                    map[i][k-1] = 1
+
+    else:
+        print('Not readying axis')
+
+    return map
+
+axis = ''
+line = 0
+folds = 0
+for i in range(len(instructions)):
+    for k in range(len(instructions[i])):
+        if instructions[i][k] == '=':
+            axis = (instructions[i][k-1])
+            line = int(instructions[i][(k+1):])-1
+            print(instructions[i])
+            map = fold(axis, line, map)
+            dots = 0
+            folds += 1
+            for j in range(len(map)):
+                for l in range(len(map[j])):
+                    if map[j][l] == 0:
+                        dots += 1
+            print('There are ' + str(dots) + ' dots after ' + str(folds) + ' folds.')
+
+
+
+
+
+# 584071 too high
+# 583178 too high
